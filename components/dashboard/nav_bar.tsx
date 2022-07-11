b611b4ce62e6d9fad4f98f71a60f4e9ada72ec5c
Allen Eben <allen.eben@fjorgedigital.com>
2022-07-11 11:58:28 +0000

    :new changes

--- components/dashboard/nav_bar.tsx
+++ components/dashboard/nav_bar.tsx
@@ -92,7 +92,6 @@ function NavBar(isOpen: boolean, onClose: () => void, onOpen: () => void) {
                                 as={motion.div}
                                 px={2}
                                 py={1}
-                                color={useColorModeValue('white', 'gray.700')}
                                 fontSize="xs"
                                 display={'flex'}
                                 flexDir="column"
